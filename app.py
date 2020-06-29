3d433e01fca622053bf00321736f550a54801063
Fernanda Cetnarovski <fernanda.cetnarovski@smartbox.com>
2020-06-29 13:36:54 +0100

    Add IP and Port on app.py

--- app.py
+++ app.py
@@ -36,3 +36,8 @@ def contact():
         print(request.form)
     return render_template("contact.html", page_title="Contact")
 
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
